d616fd47871087066c270cc65aec7adfa90863e8
Chealyfey Vutha <lyfeytech@gmail.com>
2024-03-20 13:21:05 +0700

    Fixed Database Connect

--- config/Database.php
+++ config/Database.php
@@ -16,20 +16,21 @@
             $this->host= getenv('DBHOST');
             $this->port= getenv('DBPORT');
         }
-    // DB Connect
-    public function connect() {
-        if($this->conn) {
-          return $this->conn; // connection already exists, return it
-        }
-        else{
-          $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
-          try { 
-            $this->conn = new PDO($dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->conn;
-          } catch(PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-          }
-        }
+// DB Connect
+public function connect() {
+  if($this->conn) {
+      return $this->conn; // connection already exists, return it
+  }
+  else {
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};user={$this->username};password={$this->password}";
+      try { 
+          $this->conn = new PDO($dsn);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          return $this->conn;
+      } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
       }
+  }
+}
+
     }
